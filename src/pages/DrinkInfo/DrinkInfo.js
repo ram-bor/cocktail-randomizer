0bd088a52d7f9457609d212643083b9958eaab24
RobertoRamos <robram91@gmail.com>
2021-02-12 01:20:06 -0500

    drinkinfo

--- src/pages/DrinkInfo/DrinkInfo.js
+++ src/pages/DrinkInfo/DrinkInfo.js
@@ -4,7 +4,9 @@ import './DrinkInfo.css';
 function DrinkInfo() {
   return (
     <div className="drinkinfo">
-      <h1>Drink Information will be displayed here</h1>
+      <h1>
+        Drink Information will be <i>drinksplayed</i> here
+      </h1>
       <h3>(Also on another page)</h3>
     </div>
   );
