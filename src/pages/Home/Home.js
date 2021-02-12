d174819891a46c5cc62b1751d14d2d41f4d6a37f
RobertoRamos <robram91@gmail.com>
2021-02-11 23:31:25 -0500

    changed names for better context

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,16 @@
-import './App.css';
-import Home from '../'
+import React from 'react';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header className="home-header">
         <p>
           <h1>~ Cocktail Randomizer Name or Logo Here ~</h1>
           <h2>Click here to open shaker</h2>
-          
         </p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Home;
