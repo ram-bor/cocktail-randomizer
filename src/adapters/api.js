6aaede2aaa3ee7d50c55a7d7edd76f76b69d0a2b
RobertoRamos <robram91@gmail.com>
2021-02-15 14:13:52 -0500

    api

--- src/adapters/api.js
+++ src/adapters/api.js
@@ -1,20 +1,41 @@
-import Axios from 'axios';
+// import axios from 'axios';
+// import React from 'react';
 
-url = 'https://powerful-mountain-75920.herokuapp.com/cocktails';
+// var url = 'https://powerful-mountain-75920.herokuapp.com/cocktails';
+
+// export default class Api extends React.Component {
+//   state = {
+//     drinks = []
+//   }
+
+//   componentDidMount() {
+//       axios.get(url)
+//           .then(response => {
+//             const drinks = response.data
+//             this.setState({ drinks })
+//           })
+//   }
+
+//   render() {
+//       return (
+//           <ul> {this.state.drinks.map(drinks => <li>{drinks.name}</li> )} </ul>
+//       )
+//   }
+// }
 
 // using Axios create method to instantiate  Data object
-export default axiosInstance(() => {
-  return Axios.create(initializers);
-});
+// export default axiosInstance(() => {
+//   return Axios.create(initializers);
+// });
 
-// create function to GET data from url given using function made above to create instance
-export function get(url) {
-  const axios = axiosInstance();
-  return axios.get(url);
-}
+// // create function to GET data from url given using function made above to create instance
+// export function get(url) {
+//   const axios = axiosInstance();
+//   return axios.get(url);
+//   console.log(url);
+// }
 
-export function post(url, reqData) {
-  const axios = axiosInstance();
-  return axios.post(url, reqData);
-}
-
+// export function post(url, reqData) {
+//   const axios = axiosInstance();
+//   return axios.post(url, reqData);
+// }
