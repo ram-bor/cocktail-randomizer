d43ad1f77df3606a737058a97e4048a2293a4301
RobertoRamos <robram91@gmail.com>
2021-02-15 12:19:40 -0500

    creating methods

--- src/adapters/api.js
+++ src/adapters/api.js
@@ -1,12 +1,19 @@
 import Axios from 'axios';
 
+url = 
+
+// using Axios create method to instantiate  Data object
 export default axiosInstance(() => {
   return Axios.create(initializers);
 });
 
+// create function to GET data from url given using function made above to create instance
 export function get(url) {
-    const axios = axiosInstance()
-    return axios.get(url)
+  const axios = axiosInstance();
+  return axios.get(url);
 }
 
-export 
+export function post(url, reqData) {
+  const axios = axiosInstance();
+  return axios.post(url, reqData);
+}
