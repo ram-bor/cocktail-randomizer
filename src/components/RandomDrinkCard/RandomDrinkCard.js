9a94f365ff7b7fd8b994b0421a996170d89b90d6
RobertoRamos <robram91@gmail.com>
2021-02-23 14:15:01 -0500

    more card changes

--- src/components/RandomDrinkCard/RandomDrinkCard.js
+++ src/components/RandomDrinkCard/RandomDrinkCard.js
@@ -51,18 +51,15 @@ export default class RandomDrinkCard extends Component {
   render() {
     return (
       <div class="card">
-        <img
-          src="..."
-          class="card-img"
-          style={{ width: '18rem' }}
-          alt="(Image here)"
-        />
         <div class="card-body">
-          <h4 class="card-title"></h4>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content. (Drink here)
-          </p>
+          <img
+            src="..."
+            class="card-img"
+            style={{ width: '18rem' }}
+            alt="(Image here)"
+          />
+          <h4 class="drink-name">{this.state.name}</h4>
+          <p class="drink-ingredients">{this.state.ingredients}</p>
           <a href="/" class="btn btn-danger">
             No! Choose another drink chosen
           </a>
