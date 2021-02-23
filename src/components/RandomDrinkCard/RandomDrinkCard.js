d69b618f5fdf49b9d55ca06be35fecfa7c6ffb85
RobertoRamos <robram91@gmail.com>
2021-02-23 11:53:07 -0500

    props

--- src/components/RandomDrinkCard/RandomDrinkCard.js
+++ src/components/RandomDrinkCard/RandomDrinkCard.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function RandomDrinkCard(props) {
-  return (
-    <div class="card">
-      <img
-        src="..."
-        class="card-img-top"
-        style={{ width: '18rem' }}
-        alt="(Image here)"
-      />
-      <div class="card-body">
-        <h4 class="card-title">(Drink Name)</h4>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content. (Drink here)
-        </p>
-        <a href="/" class="btn btn-danger">
-          No! Choose another drink chosen
-        </a>
-        <a href="/" class="btn btn-primary">
-          Yes! Go to specific drink
-        </a>
+import Api from '../../adapters/Api';
+
+export default class RandomDrinkCard extends Component {
+  constructor(props)
+  super(props)
+  
+  render() {
+    return (
+      <div class="card">
+        <img
+          src="..."
+          class="card-img-top"
+          style={{ width: '18rem' }}
+          alt="(Image here)"
+        />
+        <div class="card-body">
+          <h4 class="card-title">(Drink Name)</h4>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content. (Drink here)
+          </p>
+          <a href="/" class="btn btn-danger">
+            No! Choose another drink chosen
+          </a>
+          <a href="/" class="btn btn-primary">
+            Yes! Go to specific drink
+          </a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
