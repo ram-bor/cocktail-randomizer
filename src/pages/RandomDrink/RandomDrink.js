dce917e19676cc9e64916908c4c3173855b7b8f0
RobertoRamos <robram91@gmail.com>
2021-02-24 23:13:35 -0500

    adding routes to drink choice

--- src/pages/RandomDrink/RandomDrink.js
+++ src/pages/RandomDrink/RandomDrink.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './RandomDrink.css';
 import Button from '../../components/Button/Button';
 import RandomDrinkCard from '../../components/RandomDrinkCard/RandomDrinkCard';
+import DrinkChoice from '../DrinkInfo/DrinkInfo';
+import { Route, Link } from 'react-router-dom';
 
 function RandomDrink() {
+  function loadDrinkChoice() {
+    console.log('Go button clicked. Loading Drink Choice information');
+  }
   return (
     <div className="random-drink-parent">
       <div className="random-drink">
@@ -12,12 +17,19 @@ function RandomDrink() {
         </div>
         <div className="button-container">
           <div>
-            <Button />
+            <Link to="/drinks">
+              <Button />
+            </Link>
             <p>Choose again!</p>
           </div>
           <div>
-            <Button />
+            <Link to="/drinkinfo">
+              <Button />
+            </Link>
             <p>Yes please!</p>
+          </div>
+          <div>
+            <Route path="/drinkinfo" component={DrinkChoice} />
           </div>
         </div>
       </div>
