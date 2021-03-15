6cf933371c941ffcec560fbd171e413b7a201785
RobertoRamos <robram91@gmail.com>
2021-03-14 20:32:25 -0400

    bugs

--- src/pages/DrinkInfo/DrinkInfo.js
+++ src/pages/DrinkInfo/DrinkInfo.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './DrinkInfo.css';
 import DrinkCard from '../../components/DrinkCard/DrinkCard';
 
-function DrinkInfo() {
-  return (
-    <div className="drinkinfo">
-      <h1>
-        Drink Information will be <i>drinksplayed</i> here
-      </h1>
-      <h3>(last page)</h3>
-      <DrinkCard
-        icon="pic"
-        name="fuk me up fam"
-        glass="martini glass idk"
-        ingredients="poison"
-        instructions="run away"
-      />
-    </div>
-  );
+export class DrinkInfo extends Component() {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="drinkinfo">
+        <h1>
+          Drink Information will be <i>drinksplayed</i> here
+        </h1>
+        <h3>(last page)</h3>
+        <DrinkCard
+          icon="pic"
+          name="fuk me up fam"
+          glass="martini glass idk"
+          ingredients="poison"
+          instructions="run away"
+        />
+      </div>
+    );
+  }
 }
 
 export default DrinkInfo;
